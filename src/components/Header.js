8b4cbb8330e571531950ae1a51ed190107534182
jhossepmartinez <jhossepmv@gmail.com>
2026-02-28 23:15:09 -0300

    feat: add another header

--- src/components/Header.js
+++ src/components/Header.js
@@ -67,7 +67,7 @@ export default function Header(props) {
         <div className="absolute left-0 top-0">
           <Image src="/images/demo.svg" height={88} width={88} />
         </div>
-        <h1 class="font-bold py-4">HOLA</h1>
+        <h1 class="font-bold py-4">NUEVO HEADER</h1>
       </header>
 
       <Account
